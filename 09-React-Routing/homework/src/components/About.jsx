caf0e0425878a1deb6916354da799f22a3dcaad8
Felipe Guitelmane <felipe.guitelman@.com>
2022-04-26 19:14:24 -0300

    HK React Routing

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <h4>Sobre mi..</h4>
+        </div>
+    )
+}
--- 09-React-Routing/homework/src/containers/App.js
+++ 09-React-Routing/homework/src/containers/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import Nav from '../components/Nav.jsx';
 import Cards from '../components/Cards.jsx';
+import About from '../components/About.jsx';
+import Ciudad from '../components/Ciudad.jsx';
+
 
 const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
 
@@ -37,23 +41,27 @@ function App() {
       });
   }
   function onFilter(ciudadId) {
-    let ciudad = cities.filter(c => c.id === parseInt(ciudadId));
-    if(ciudad.length > 0) {
-        return ciudad[0];
-    } else {
-        return null;
-    }
+    let ciudad = cities.find(c => c.id === parseInt(ciudadId));
+    return ciudad;
   }
   return (
     <div className="App">
-      <Nav onSearch={onSearch}/>
-      <div>
-        <Cards
-          cities={cities}
-          onClose={onClose}
-        />
-      </div>
-      <hr />
+      <Route 
+        path='/'
+        render={() => <Nav onSearch={onSearch}/>}
+      />
+      <Route
+        exact path='/'
+        render={() => <Cards cities={cities} onClose={onClose} />}
+      />
+      <Route
+        path='/about'
+        component={About}
+      />
+       <Route
+        exact path='/ciudad/:ciudadId'
+        render={({match}) => <Ciudad city={onFilter(match.params.ciudadId)}/>}
+      />
     </div>
   );
 }
